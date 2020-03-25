fee29423a40ea4b9d81ae1a94c46851b02d2c175
joaquim <joaquimpineda@gmail.com>
2020-03-25 16:59:06 +0100

    not working

--- router/chat.js
+++ router/chat.js
@@ -4,17 +4,56 @@ const path = require('path');
 const User = require('../models/user');
 
 
-router.get('/', (req, res) => {
-  User.findById(req.session.currentUser._id)
-    
-    .then(response => {
-      console.log(response)
-      let chat = JSON.stringify(response.chat)
-      res.render('chat/chat', { response, chat })
+router.get('/', async (req, res) => {
+  response = await User.findById(req.session.currentUser._id)
 
-    })
+
+  const anAsyncFunction = async obj => {
+    let user = await User.findById(obj.user).populate("user");
+    return { name: user.name, chatId: obj.chatId }
+  }
+  const getData = async () => {
+    return Promise.all(response.chat.map(item => anAsyncFunction(item)))
+  }
+  getData().then(data => {
+    console.log(data)
+    let chat = JSON.stringify(data)
+    res.render('chat/chat', { response, chat })
+  })
+
+
+
+
+
+
+
+  //console.log(response.chat)
+
+
+
   // res.sendFile(path.join(__dirname, "../public/html/index.html"))
 })
 
 
 module.exports = router;
+
+
+/* try{
+  const userLog = req.session.currentUser;
+  const likeList = userLog.likeList;
+    const anAsyncFunction = async obj => {
+      let user = await User.findById(obj.userwhoLikes);
+      let product = await Product.findById(obj.productLiked);
+      return {user, product}
+    }
+    const getData = async () => {
+      return Promise.all(likeList.map(item => anAsyncFunction(item)))
+    }
+    getData().then(fullLikeList => {
+      res.render("notifications.hbs", { fullLikeList});
+    })
+}catch(err){
+  console.log(err)
+  next(err)
+}
+*/
